72166028ff392be752b179511f1eb575376ad41b
Franco Martucci <franco.martucci.21@gmail.com>
2022-08-31 10:56:30 -0300

    Add camera movement and level limits (so that the camera does not enter the void)

---
+++ SunsetRiders/Assets/Scripts/StageHandler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageHandler : MonoBehaviour
+{
+    public GameObject min;
+    public GameObject max;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
