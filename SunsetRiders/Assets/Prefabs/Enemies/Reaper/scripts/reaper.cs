be5ed83bbb531560a3b8514dc5c47a58caa9c30b
Franco Martucci <franco.martucci.21@gmail.com>
2022-09-02 19:26:05 -0300

    Add first enemy to the game: Reaper(bot)! Add enemy spawns and make them spawn from time to time. Make some minor tweaks to other aspects of the game.

---
+++ SunsetRiders/Assets/Prefabs/Enemies/Reaper/scripts/reaper.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reaper : MonoBehaviour
+{
+    public GameObject bullet;
+    public float movementSpeed;
+    public float cooldown;
+
+    GameObject player;
+    GameObject sprite;
+    Animator animator;
+    Vector3 stageMax;
+    Vector3 stageMin;
+    Vector2 vel = new Vector2(0, 0);
+    bool isGettingClose = true;
+    bool isCooldown = false;
+    bool isFireAnimationActive = false;
+    bool isDead = false;
+
+    void Start()
+    {
+        StageHandler stage = GameObject.FindGameObjectWithTag("Stage").GetComponent<StageHandler>();
+        stageMax = stage.max.transform.position;
+        stageMin = stage.min.transform.position;
+
+        player = GameObject.Find("gunner");
+        sprite = gameObject.transform.Find("reaper_sprite").gameObject;
+        animator = sprite.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (isDead)
+        {
+            vel.x *= 0.8f;
+            return;
+        }
+
+        float optimalDistance = isGettingClose ? 3.8f : 4.8f;
+
+        Vector2 distance = player.gameObject.transform.position - gameObject.transform.position;
+        if (distance.sqrMagnitude > optimalDistance)
+        {
+            isGettingClose = true;
+            vel = distance.normalized * movementSpeed;
+        }
+        else
+        {
+            isGettingClose = false;
+            vel = distance.normalized * -movementSpeed / 4f;
+            if (!isCooldown)
+            {
+                isCooldown = true;
+                isFireAnimationActive = true;
+                animator.Play("reaper_fire");
+                Invoke("resetFireAnimation", cooldown / 3f);
+                Invoke("resetCooldown", cooldown);
+                GameObject newBullet = Instantiate(bullet, gameObject.transform.position, Quaternion.identity);
+                newBullet.GetComponent<reaper_bullet>().setDirection(distance.normalized);
+            }
+        }
+
+        if (!isFireAnimationActive)
+        {
+            if (Mathf.Abs(vel.x) > 0.2)
+            {
+                animator.Play("reaper_moving");
+                sprite.GetComponent<SpriteRenderer>().flipX = vel.x > 0;
+            }
+            else
+            {
+                animator.Play("reaper_idle");
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isDead)
+        {
+            vel.y += Physics2D.gravity.y * Time.deltaTime;
+        }
+        GetComponent<Rigidbody2D>().position += vel * Time.deltaTime;
+        checkLimits();
+    }
+
+    void checkLimits()
+    {
+        float yPadTop = 0.15f;
+        float yPadBot = 0.15f / 2f;
+        if (GetComponent<Rigidbody2D>().position.y + yPadTop > stageMax.y)
+        {
+            GetComponent<Rigidbody2D>().position = new Vector2(GetComponent<Rigidbody2D>().position.x, stageMax.y - yPadTop);
+        }
+        else if (GetComponent<Rigidbody2D>().position.y - yPadBot < stageMin.y)
+        {
+            GetComponent<Rigidbody2D>().position = new Vector2(GetComponent<Rigidbody2D>().position.x, stageMin.y + yPadBot);
+        }
+    }
+
+    void resetFireAnimation()
+    {
+        isFireAnimationActive = false;
+    }
+
+    void resetCooldown()
+    {
+        isCooldown = false;
+    }
+
+    public void getHit()
+    {
+        isDead = true;
+        animator.Play("reaper_dead");
+        Destroy(gameObject, 1f);
+    }
+}
--- SunsetRiders/Assets/Scripts/StageHandler.cs
+++ SunsetRiders/Assets/Scripts/StageHandler.cs
@@ -6,14 +6,33 @@ public class StageHandler : MonoBehaviour
 {
     public GameObject min;
     public GameObject max;
+    public GameObject reaper;
+    public GameObject[] enemySpawns;
 
     void Start()
     {
-        
+        enemySpawns = new GameObject[3] {
+            GameObject.Find("stage/enemy_spawns/spawn1"),
+            GameObject.Find("stage/enemy_spawns/spawn2"),
+            GameObject.Find("stage/enemy_spawns/spawn3"),
+        };
+        spawnEnemies();
     }
 
     void Update()
     {
-        
+
+    }
+
+    void spawnEnemies()
+    {
+        foreach (GameObject spawn in enemySpawns)
+        {
+            if (Random.Range(0f, 1f) > 0.5f)
+            {
+                Instantiate(reaper, spawn.transform.position, Quaternion.identity);
+            }
+        }
+        Invoke("spawnEnemies", Random.Range(10f, 15f));
     }
 }
